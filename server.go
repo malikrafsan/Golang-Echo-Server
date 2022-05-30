4fd22863e8965f7afffc2260fc55eff1380d980b
Malik Rafsanjani <13520105@std.stei.itb.ac.id>
2022-05-30 22:46:34 +0700

    init env var

--- server.go
+++ server.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
